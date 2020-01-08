7287ae99b2fb5565829b54f03943761c5f1f4ec8
Stefan Miletic <stefan.miletic@seavus.com>
2020-01-08 13:07:40 +0100

    staff

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"
+  shared_credentials_file = "/.aws/crednetials"
+  profile = "seavus"
+}
+resource "aws_instance" "coolInstnace" {
+  ami = "ami-0e55e373"
+  instance_type = "t1.micro"
+  tags {
+      Name = "StefanVM"
+  }
+}
